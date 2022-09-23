e605115da957840cc31f0a54befb50e58de6a9a0
mateuskrause <mateus.ke@outlook.com>
2022-09-23 10:24:05 -0300

    shader funcionando com mais de uma luz simples

--- shader.glsl
+++ shader.glsl
@@ -2,9 +2,11 @@
 
 // x, y position of the light
 uniform vec2 lightPosition;
+uniform vec2 lightPosition2;
 // Size of light in pixels
 uniform float lightSize;
 
+// basicamente verificando as paredes do chanel0
 float terrain(vec2 samplePoint)
 {
     float samplePointAlpha = texture(iChannel0, samplePoint).a;
@@ -20,35 +22,82 @@ float terrain(vec2 samplePoint)
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
+
     // Distance in pixels to the light
     float distanceToLight = length(lightPosition - fragCoord);
+    float distanceToLight2 = length(lightPosition2 - fragCoord);
+
+    // if (distanceToLight2 < distanceToLight) {
+    //     distanceToLight = distanceToLight2;
+    // }
+
 
     // Normalize the fragment coordinate from (0.0, 0.0) to (1.0, 1.0)
     vec2 normalizedFragCoord = fragCoord/iResolution.xy;
-    vec2 normalizedLightCoord = lightPosition.xy/iResolution.xy;
 
     // Start our mixing variable at 1.0
     float lightAmount = 1.0;
-    for(float i = 0.0; i < N; i++)
-    {
-        // A 0.0 - 1.0 ratio between where our current pixel is, and where the light is
-        float t = i / N;
-        // Grab a coordinate between where we are and the light
-        vec2 samplePoint = mix(normalizedFragCoord, normalizedLightCoord, t);
-        // Is there something there? If so, we'll assume we are in shadow
-	    float shadowAmount = terrain(samplePoint);
-        // Multiply the light amount.
-        // (Multiply in case we want to upgrade to soft shadows)
-        lightAmount *= shadowAmount;
-    }
+    float lightAmount2 = 1.0;
+
+    float shadowAmount = terrain(normalizedFragCoord);
+    lightAmount *= shadowAmount;
 
     // Find out how much light we have based on the distance to our light
     lightAmount *= 1.0 - smoothstep(0.0, lightSize, distanceToLight);
+    lightAmount2 *= 1.0 - smoothstep(0.0, lightSize, distanceToLight2);
+
+    // // Normalize the fragment coordinate from (0.0, 0.0) to (1.0, 1.0)
+    // vec2 normalizedFragCoord = fragCoord/iResolution.xy;
+    // vec2 normalizedLightCoord = lightPosition.xy/iResolution.xy;
+
+    // // Start our mixing variable at 1.0
+    // float lightAmount = 1.0;
+    // for(float i = 0.0; i < N; i++)
+    // {
+    //     // A 0.0 - 1.0 ratio between where our current pixel is, and where the light is
+    //     float t = i / N;
+    //     // Grab a coordinate between where we are and the light
+    //     vec2 samplePoint = mix(normalizedFragCoord, normalizedLightCoord, t);
+    //     // Is there something there? If so, we'll assume we are in shadow
+	//     float shadowAmount = terrain(samplePoint);
+    //     // Multiply the light amount.
+    //     // (Multiply in case we want to upgrade to soft shadows)
+    //     lightAmount *= shadowAmount;
+    // }
+
+    // Find out how much light we have based on the distance to our light
+    // lightAmount *= 1.0 - smoothstep(0.0, lightSize, distanceToLight);
+
+    // --
+    // vec2 lightPosition2;
+    // lightPosition2.x = 320;
+    // lightPosition2.y = 320;
+    // normalizedFragCoord = fragCoord/iResolution.xy;
+    // normalizedLightCoord = lightPosition2.xy/iResolution.xy;
+    // float lightAmount2 = 1.0;
+    // for(float i = 0.0; i < N; i++)
+    // {
+    //     // A 0.0 - 1.0 ratio between where our current pixel is, and where the light is
+    //     float t = i / N;
+    //     // Grab a coordinate between where we are and the light
+    //     vec2 samplePoint = mix(normalizedFragCoord, normalizedLightCoord, t);
+    //     // Is there something there? If so, we'll assume we are in shadow
+	//     float shadowAmount = terrain(samplePoint);
+    //     // Multiply the light amount.
+    //     // (Multiply in case we want to upgrade to soft shadows)
+    //     lightAmount2 *= shadowAmount;
+    // }
+
+    // lightAmount2 *= 1.0 - smoothstep(0.0, lightSize, distanceToLight);
+
+    // if (lightAmount2 < lightAmount) {
+    //     lightAmount = lightAmount2;
+    // }
 
     // We'll alternate our display between black and whatever is in channel 1
     vec4 blackColor = vec4(0.0, 0.0, 0.0, 1.0);
 
     // Our fragment color will be somewhere between black and channel 1
     // dependent on the value of b.
-    fragColor = mix(blackColor, texture(iChannel1, normalizedFragCoord), lightAmount);
+    fragColor = mix(blackColor, texture(iChannel1, normalizedFragCoord), lightAmount + lightAmount2);
 }
